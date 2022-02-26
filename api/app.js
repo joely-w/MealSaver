d507f6a375d6a26cda6dd597bea90f29d28c4a7d
Dap2002 <dylanpoole2002@outlook.com>
2022-02-26 15:07:48 +0000

    initial commit

--- api/app.js
+++ api/app.js
@@ -1,3 +1,15 @@
 /**
  * API for Meal Saver
  */
+
+const http = require('http');
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.json());
+app.use(express.static(__dirname+"/../ui"));
+// default URL for website
+const server = http.createServer(app);
+const port = 3000;
+server.listen(port);
+console.debug('Server listening on port ' + port);
