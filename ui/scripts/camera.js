ff3ac308029093c20c7ff8e17b033b53b4a1aa36
Joel Winterton <joel.winterton@outlook.com>
2022-02-26 21:18:00 +0000

    Styled buttons

--- ui/scripts/camera.js
+++ ui/scripts/camera.js
@@ -1,22 +1,23 @@
-const video = document.getElementById('video');
-
+const video = $("#video")
+const canvas = $("#canvas")
 // Get access to the camera!
-if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
     // Not adding `{ audio: true }` since we only want video now
-    navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+    navigator.mediaDevices.getUserMedia({video: true}).then(function (stream) {
         //video.src = window.URL.createObjectURL(stream);
-        video.srcObject = stream;
-        video.play();
+        video[0].srcObject = stream;
+        video[0].play();
     });
 }
 // Elements for taking the snapshot
-const canvas = document.getElementById('canvas');
-const context = canvas.getContext('2d');
+const context = canvas[0].getContext('2d');
 
 // Trigger photo take
-document.getElementById("snap").addEventListener("click", function()
-{
-    video.style.display = "none"
-    canvas.style.display = "block"
-    context.drawImage(video, 0, 0, 640, 480);
+document.getElementById("snap").addEventListener("click", function () {
+    $("#snap").hide();
+
+    video.hide()
+    canvas.show();
+    context.drawImage(video[0], 0, 0, 640, 480);
+
 });
