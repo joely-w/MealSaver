a9032012cff1d07a4cf87f9817cb2e39d1855d25
Joel Winterton <joel.winterton@outlook.com>
2022-02-27 03:10:27 +0000

    Endpoints created

--- api/app.js
+++ api/app.js
@@ -4,14 +4,33 @@
 
 const http = require('http');
 const express = require('express');
-const path = require('path');
+const multer = require('multer');
+const upload = multer();
+const fs = require('fs');
+const bodyParser = require('body-parser')
 const app = express();
 app.use(express.json());
-app.use(express.static(__dirname+"/../ui"));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(express.static(__dirname + "/../ui"));
 const dbConn = new (require("./services/database.js"))();
 const ocr = new (require("./services/recieptOcr"))();
 // default URL for website
 const server = http.createServer(app);
+app.post('/api/upload/receipt', upload.single('file'), async (req, res) => {
+    const filename = `./tmp/${(Math.random() + 1).toString(36).substring(7)}.jpeg`;
+    await fs.writeFile(filename, req.file.buffer, async (err) => {
+        if (err) return res.status(500).send('Error uploading image')
+        const result = await ocr.parseReceipt(filename)
+        return res.send(result);
+    })
+});
+app.post('/api/save/inventory', async (req, res) => {
+    await ocr.addToInventory(req.body)
+    // TODO Needs to send a user parameter when sessions are implemented
+    res.status(200).send('OK');
+})
 const port = 3001;
 server.listen(port);
 console.debug('Server listening on port ' + port);
--- api/services/recieptOcr.js
+++ api/services/recieptOcr.js
@@ -1,28 +1,28 @@
 require("formidable");
-const { ocrSpace } = require('ocr-space-api-wrapper');
+const {ocrSpace} = require('ocr-space-api-wrapper');
 const fs = require("fs");
 const csv = require("csv-parser");
 var stringSimilarity = require("string-similarity");
 const Database = require("./database");
 
-module.exports = class OCR{
+module.exports = class OCR {
 
     itemsArray = [];
 
-    constructor(){
-        console.log("here");
+    constructor() {
+        //console.log("here");
         this.put_in_array();
-        this.parseReceipt("https://fastly.4sqi.net/img/general/600x600/47596406_O6U_qqwQOjXYqsG8xAMOO0x805hHreZMXf6R1WNma5I.jpg")
-            .then(r => console.log(r));
+        //this.parseReceipt("https://fastly.4sqi.net/img/general/600x600/47596406_O6U_qqwQOjXYqsG8xAMOO0x805hHreZMXf6R1WNma5I.jpg")
+        //    .then(r => console.log(r));
     }
 
-    put_in_array(){
+    put_in_array() {
         const csv = require('csv-parser');
         const fs = require('fs');
         fs.createReadStream('Groceries_dataset.csv')
             .pipe(csv())
             .on('data', (row) => {
-                if (!(this.itemsArray).includes(row["itemDescription"].toLowerCase())){
+                if (!(this.itemsArray).includes(row["itemDescription"].toLowerCase())) {
                     this.itemsArray.push(row["itemDescription"].toLowerCase());
                 }
             })
@@ -31,7 +31,7 @@ module.exports = class OCR{
             });
     }
 
-    levenshteinDistance (str1 = '', str2 = ''){
+    levenshteinDistance(str1 = '', str2 = '') {
         const track = Array(str2.length + 1).fill(null).map(() =>
             Array(str1.length + 1).fill(null));
         for (let i = 0; i <= str1.length; i += 1) {
@@ -53,52 +53,50 @@ module.exports = class OCR{
         return track[str2.length][str1.length];
     };
 
-    async parseReceipt (imagePath) {
-        try {
-            // Using your personal API key + local file
-            let receiptItems = [];
-            const res = await ocrSpace(imagePath, { apiKey: 'K83288537388957' }); // K82372313488957 K81413160288957 K88956093988957
-            //do something with response here.
-            const receiptText = (res["ParsedResults"][0]["ParsedText"]).toLowerCase();
-            console.log(receiptText);
-            const words = receiptText.split("\n");
-            let bestMatchString;
-            for (let i in words){
-                let bestMatch = 0;
-                let currentMatch;
-                for (let j in this.itemsArray){
-                    currentMatch = stringSimilarity.compareTwoStrings(words[i], this.itemsArray[j]);
-                    if (currentMatch > bestMatch){
-                        bestMatch = currentMatch;
-                        bestMatchString = this.itemsArray[j];
-                    }
-                }
-                if (this.levenshteinDistance(bestMatchString, words[i]) < 3){
-                    if(receiptItems[bestMatchString]) receiptItems[bestMatchString]++;
-                    else receiptItems[bestMatchString] = 1;
+    async parseReceipt(imagePath) {
+        // Using your personal API key + local file
+        let receiptItems = {};
+        console.log('Hanging')
+        const res = await ocrSpace('https://img.search.brave.com/DmSg1aj42yveYskRxwhWXpJ23998KzmqUJk4fgpRU1E/rs:fit:1200:655:1/g:ce/aHR0cHM6Ly93d3cu/ZXhwcmVzc2FuZHN0/YXIuY29tL3Jlc2l6/ZXIvdFVHVGpCZE82/czl2SmtrRDZ2N18w/TDh1TUNrPS8xMjAw/eDAvY2xvdWRmcm9u/dC11cy1lYXN0LTEu/aW1hZ2VzLmFyY3B1/Ymxpc2hpbmcuY29t/L21uYS80MzJOSTVH/T001RjRSSzRITTVO/RVlTSDZEUS5qcGc', {apiKey: 'K82372313488957'}); //  K81413160288957 K88956093988957 K83288537388957
+        console.log('Unhung', res)
+        //do something with response here.
+        const receiptText = (res["ParsedResults"][0]["ParsedText"]).toLowerCase();
+        const words = receiptText.split("\r\n");
+        console.log(words)
+        let bestMatchString;
+        for (let i in words) {
+            let bestMatch = 0;
+            let currentMatch;
+            for (let j in this.itemsArray) {
+                currentMatch = stringSimilarity.compareTwoStrings(words[i], this.itemsArray[j]);
+                if (currentMatch > bestMatch) {
+                    bestMatch = currentMatch;
+                    bestMatchString = this.itemsArray[j];
                 }
             }
-            //TODO handle item validation at this point
-            for (let x in receiptItems){
-                for (let y=0; y<=receiptItems[x]; y++){
-                    //TODO update user_id here!
-                    //get id of item in DB
-                    console.log("X: "+x);
-                    Database.conn.query("SELECT item_id FROM items WHERE title LIKE'%"+x+"%'", function (err, result, fields) {
-                        if (err) throw err;
-                        console.log(result);
-                        let id = result[0]["item_id"];
-                        Database.conn.query("INSERT INTO items_addition (item_id, user_id, date) VALUES ("+id+", 1,  now())", function (err, result, fields) {
-                            if (err) throw err;
-                            console.log("inserted into database!");
-                        });
-                        //console.log(result);
-                    });
-                }
+            if (this.levenshteinDistance(bestMatchString, words[i]) < 3) {
+                if (receiptItems[bestMatchString]) receiptItems[bestMatchString]++;
+                else receiptItems[bestMatchString] = 1;
             }
-        } catch (error) {
-            console.error(error);
+        }
+        console.log(receiptItems)
+        return receiptItems;
+    }
+
+    async addToInventory(receiptItems) {
+        for (let x in receiptItems) {
+            if (!receiptItems.hasOwnProperty(x)) {
+                continue
+            }
+            for (let y = 0; y <= receiptItems[x]; y++) {
+                //TODO update user_id here!
+                //get id of item in DB
+                const result = await Database.asyncQuery(`SELECT item_id FROM items WHERE title LIKE'%${x}%'`)
+                let id = result[0]["item_id"];
+                await Database.asyncQuery("INSERT INTO items_addition (item_id, user_id, date) VALUES (" + id + ", 1,  now())")
+            }
         }
     }
+
 
 }
