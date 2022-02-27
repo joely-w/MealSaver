8fbc8bec18d6b8273577e4805a7f671efbd0d896
Joel Winterton <joel.winterton@outlook.com>
2022-02-27 03:12:25 +0000

    hooked up to API endpoints

--- ui/scripts/camera.js
+++ ui/scripts/camera.js
@@ -1,5 +1,5 @@
 const video = $("#video")
-const canvas = $("#canvas")
+const canvas = $("#canvas");
 // Get access to the camera!
 if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
     // Not adding `{ audio: true }` since we only want video now
@@ -31,5 +31,43 @@ $("#retry").click(() => {
     canvas.hide()
 })
 $("#accept").click(() => {
+    canvas[0].toBlob(async (blob) => {
+        const form_data = new FormData();
+        form_data.append('file', blob);
+        $.ajax({
+            type: 'POST',
+            contentType: false,
+            cache: false,
+            processData: false,
+            url: '/api/upload/receipt',
+            data: form_data,
+            success: data => loadInventory(data)
+        })
+    });
 
 })
+
+function loadInventory(data) {
+
+    let i = 0;
+    for (const key of Object.keys(data)) {
+        i += 1;
+        $("#items").append(`<label>${i}. <span>${key}</span><input name="${key}" type="number" value="${data[key]}"><a><i onclick="$(this).closest('label').remove()" class="fa fa-trash-o" aria-hidden="true"></i></a></label>`)
+    }
+    $("#inventory").show();
+    $("#receipt").hide();
+}
+
+$("#save").click(() => {
+    $.ajax({
+        type: "POST",
+        url: "/api/save/inventory",
+        data: $("#items").serialize(),
+        success: (res) => {
+            console.log(res)
+        }
+
+    })
+})
+// TODO add search to add elements in the list
+// TODO add back button on check list
